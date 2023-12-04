11c8a99a3ff5abea28f8ae99ea6e35fe1ec738a2
bibin k b <bibinkochumalayil007@gmail.com>
2023-12-04 23:34:51 +0530

    home page added

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -5,6 +5,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Git'),
+        centerTitle:  true,
+      ),
+      body: ListView.builder(itemBuilder: ),
+    );
   }
 }
