cf1c0cb56ef8176a94f8e9f0e364f7f21e346692
bibin k b <bibinkochumalayil007@gmail.com>
2023-12-05 23:47:56 +0530

    made some changes on newbranch

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:git/view/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
-     home: HomePage(),
+
     );
   }
 }
